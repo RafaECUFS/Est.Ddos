c5657447e019b8ff97503e92db265744a0f0541a
Rafael Miranda <94235393+RafaECUFS@users.noreply.github.com>
2023-10-09 14:08:30 -0300

    Update arvore_heap.c

--- arvore_heap.c
+++ arvore_heap.c
@@ -1,194 +1,142 @@
-#include <stdint.h>
 #include <stdio.h>
-#include <string.h>
+#include <stdint.h>
 #include <stdlib.h>
+#include <string.h>
 
-typedef struct NO{
+typedef struct aposta{
     int acertos;
-    char* codigo_aposta;
+    char codigo_aposta[33];
+}bet;
 
-}NO;
-
-void trocar (NO** V, int Pai , int i){
-    NO* Valor_pai = (NO*)calloc(1,sizeof(NO));
-    NO* Valor_atual = (NO*)calloc(1,sizeof(NO));
-    Valor_pai -> codigo_aposta = (char*) calloc(33,sizeof(char));
-    Valor_atual -> codigo_aposta = (char*) calloc(33,sizeof(char));
-    Valor_atual ->acertos = V[i]->acertos;
-    Valor_pai ->acertos= V[Pai]->acertos;
-    V[Pai]->acertos = Valor_atual->acertos;
-    V[i]->acertos= Valor_pai->acertos;
-    strcpy(Valor_atual->codigo_aposta,V[i]->codigo_aposta);
-    strcpy(Valor_pai -> codigo_aposta,V[Pai]->codigo_aposta);
-    strcpy(V[Pai]->codigo_aposta,Valor_atual->codigo_aposta);
-    strcpy(V[i]-> codigo_aposta,Valor_pai->codigo_aposta);
-}
-//fazer heapify depois de constrir os heaps
-void heapify_max ( NO** V , int Tam , int i ) {
- // Declaração dos índices
-  int P = i;
-  int E = (2*i)+1;
-  int D = (2*i)+2;
-  if ( E < Tam && (V[E]-> acertos) > (V[P]->acertos)){ P = E ;}
- // Filho da direita é maior
-  if ( D < Tam && (V[D]-> acertos) > (V[P]->acertos)){P = D ;}
-// Troca e chamada recursiva
-  if ( P != i ) {
-     trocar (V , P , i );}
-  if (i>0){
-     heapify_max (V , Tam , i-1 );}
-}
-void heapify_min ( NO** V , int Tam , int i ) {
- // Declaração dos índices
-  int P = i;
-  int E = (2*i)+1;
-  int D = (2*i)+2;
-// Filho da esquerda é maior
-  if ( E < Tam && (V[E]-> acertos) < (V[P]->acertos)){ P = E ;}
- // Filho da direita é maior
-  if ( D < Tam && (V [D]->acertos) < (V [P]->acertos)){P = D ;}
-// Troca e chamada recursiva
-  if ( P != i ) {
-  trocar (V , P , i ) ;}
-  if (i>0){
-  heapify_min (V , Tam , i-1 );}
+void trocar(bet* apostas, int pai, int filho) {//passe o array como parametro na main
+    bet Valor_pai;
+    strcpy(Valor_pai.codigo_aposta, apostas[filho].codigo_aposta);
+    Valor_pai.acertos = apostas[filho].acertos;
+    strcpy(apostas[filho].codigo_aposta, apostas[pai].codigo_aposta);
+    apostas[filho].acertos = apostas[pai].acertos;
+    strcpy(apostas[pai].codigo_aposta, Valor_pai.codigo_aposta);
+    apostas[pai].acertos = Valor_pai.acertos;
 }
 
-void extrair_MAX(NO** V, int indice_apostas, NO** Ganhadores_max, int indice_ganhadores ){
-    Ganhadores_max[indice_ganhadores] = (NO*)calloc(1,sizeof(NO));
-    Ganhadores_max[indice_ganhadores]->codigo_aposta= (char*) calloc(33,sizeof(char));
-    strcpy((Ganhadores_max[indice_ganhadores]->codigo_aposta),(V[0]->codigo_aposta));
-    Ganhadores_max[indice_ganhadores]->acertos=V[0]->acertos;
-    trocar(V,0,indice_apostas);
-    V[indice_apostas]='\0';
-    indice_apostas-=1;
-    int Pai= (indice_apostas-1)/2;
-    heapify_max(V,indice_apostas,Pai);
-}
-void extrair_MIN(NO** V, int indice_apostas, NO** Ganhadores_min, int indice_ganhadores ){
-    Ganhadores_min[indice_ganhadores] = (NO*)calloc(1,sizeof(NO));
-    Ganhadores_min[indice_ganhadores]->codigo_aposta= (char*) calloc(33,sizeof(char));
-    strcpy((Ganhadores_min[indice_ganhadores]->codigo_aposta),(V[0]->codigo_aposta));
-    Ganhadores_min[indice_ganhadores]->acertos=V[0]->acertos;
-    printf("%s \n",V[0]->codigo_aposta);
-    trocar(V,0,indice_apostas);
-    V[indice_apostas]='\0';
-    indice_apostas-=1;
-    int Pai= (indice_apostas-1)/2;
-    heapify_min(V,indice_apostas,Pai);
-}
+void heapify_max ( bet* apostas, int Tam_array , int indice ){
+    // Declaração dos índices
+  int Pai = indice;
+  int Esq = (2*indice)+1;
+  int Dir = (2*indice)+2;
+  int maior = Pai;
+    if (Esq < Tam_array && apostas[Esq].acertos > apostas[maior].acertos) {
+        maior = Esq;
+    }
+    if (Dir < Tam_array && apostas[Dir].acertos > apostas[maior].acertos) {
+        maior = Dir;
+    }
 
+    // If menor is not the parent, swap and recursively call heapify_min
+    if (maior != Pai) {
+        trocar(apostas, Pai, maior);
+        heapify_max(apostas, Tam_array, maior);
+    }}
+void heapify_min(bet* apostas, int Tam_array, int indice) {
+    int Pai = indice;
+    int Esq = 2 * indice + 1;
+    int Dir = 2 * indice + 2;
 
-//solução: extrair da raiz, fazer heap para subir o maior valor e parar quando nao for mais igual ao maior
+    // Find the smallest among parent, left child, and right child
+    int menor = Pai;
+    if (Esq < Tam_array && apostas[Esq].acertos < apostas[menor].acertos) {
+        menor = Esq;
+    }
+    if (Dir < Tam_array && apostas[Dir].acertos < apostas[menor].acertos) {
+        menor = Dir;
+    }
+
+    // If menor is not the parent, swap and recursively call heapify_min
+    if (menor != Pai) {
+        trocar(apostas, Pai, menor);
+        heapify_min(apostas, Tam_array, menor);
+    }}
+void remover(bet* apostas, FILE* output, int tam){//fazer loop para cada heap
+    fprintf(output,"%s\n",apostas[0].codigo_aposta);
+    strcpy(apostas[0].codigo_aposta,apostas[tam-1].codigo_aposta);
+    apostas[0].acertos=apostas[tam-1].acertos;//remover ultima posição
+}
+int calcula_acertos(int escolhas[15], int sorteados[10]){
+        int acertos = 0;
+        for(int numero=0; numero<15; numero++){
+            for(int sort_num=0; sort_num<10; sort_num++){
+                if(escolhas[numero]==sorteados[sort_num]){
+                    acertos++;
+                    break;
+                }
+            }}
+            return acertos;
+    }
+
 int main(int argc, char* argv[]){
+    //precisa de dois heaps(um max e um minimo)
+    int premio_total,premio_max,premio_min,total_apost,tam_max=0,tam_min=0,max_acert=0,min_acert=10, tam_heap_main_iter;
+    int sort[10], apostado[15];
+    FILE* input = fopen(argv[1], "r");
+    if (input == NULL) {
+        printf("ERRO!");
+        return 0;
+    }
 
-  FILE* input = fopen(argv[1], "r");
-  if(input == NULL){printf("ERRO!");
+    FILE* output = fopen(argv[2], "w");
+    if (output == NULL) {
+        printf("ERRO!");
+        fclose(input);
+        return 0;
+    }
+    
+    fscanf(input, "%d", &premio_total);
+    premio_max = premio_min = premio_total/2;
+    fscanf(input, "%d", &total_apost);
+    bet* heap_main = (bet*)malloc(sizeof(bet)*total_apost);
+    if (heap_main == NULL) {
+        printf("ERRO!");
+        return 1;
+    }
+    tam_heap_main_iter=total_apost;
+    fscanf(input, "%d %d %d %d %d %d %d %d %d %d", &sort[0],&sort[1],&sort[2],&sort[3],&sort[4],&sort[5],&sort[6],&sort[7],&sort[8],&sort[9]);
+        
+    for(int cont=0; cont<total_apost; cont++){
+        fscanf(input, "%s %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d", heap_main[cont].codigo_aposta, &apostado[0],&apostado[1],&apostado[2],&apostado[3],&apostado[4],&apostado[5],&apostado[6],&apostado[7],&apostado[8],&apostado[9],&apostado[10],&apostado[11],&apostado[12],&apostado[13],&apostado[14]);
+        heap_main[cont].acertos = calcula_acertos(apostado,sort);
+        //calcula tamanho dos heaps max_min
+        if(heap_main[cont].acertos<=min_acert){if(heap_main[cont].acertos==min_acert){tam_min+=1;}
+        else{tam_min=1;
+        min_acert=heap_main[cont].acertos;
+        }}
+        
+        if(heap_main[cont].acertos>=max_acert){if(heap_main[cont].acertos==max_acert){tam_max+=1;}
+        else{tam_max=1;
+        max_acert=heap_main[cont].acertos;}
+        }
+        
+        }
+
+
+    for(int pai = (total_apost-1)/2; pai >= 0; pai--) {
+        heapify_max(heap_main, total_apost, pai);
+    }
+    fprintf(output,"[%d:%d:%d]\n",tam_max,max_acert,(premio_max/tam_max));
+    for(int maxim_ganhador=0; maxim_ganhador<tam_max; maxim_ganhador++){
+        remover(heap_main,output,tam_heap_main_iter);
+        tam_heap_main_iter--;
+        heapify_max(heap_main,tam_heap_main_iter,0);
+    }
+    for(int pai = (tam_heap_main_iter-1)/2; pai >= 0; pai--) {
+        heapify_min(heap_main, tam_heap_main_iter, pai);
+    }
+    fprintf(output,"[%d:%d:%d]\n",tam_min,min_acert,(premio_min/tam_min));
+    for(int min_ganhador=0; min_ganhador<tam_min; min_ganhador++) {
+        remover(heap_main,output,tam_heap_main_iter);
+        tam_heap_main_iter--;
+        heapify_min(heap_main,tam_heap_main_iter,0);
+    }
+
+    fclose(output);
     fclose(input);
-    return 0;}
-  FILE* output = fopen(argv[2], "w");
-  int premio=0, qt_apostadores=0, num_apostado=0,maior=0,menor=0,indice_ganhadores=0;
-  int sorteados[10];
-  int tam_max=0,tam_min=0, N_ganhadores_max = 0, N_ganhadores_min = 0;
-  fscanf(input,"%d",&premio);
-  fscanf(input,"%d",&qt_apostadores);
-  
-  for(int i=0;i<10;i++){
-    fscanf(input,"%d",&sorteados[i]);
-  }
-
-  NO** Vetor_MAX =(NO**) calloc(qt_apostadores,sizeof(NO*));
-  NO** Vetor_MIN = (NO**) calloc(qt_apostadores,sizeof(NO*));
-  char* entrada = (char*)calloc(33,sizeof(char));
-  int indice=0,pontuacao=0,num_maior=0,num_menor=15;
-  //pegando entrada
-
-  while(fscanf(input, "%s", entrada)==1){
-    //alocando memoria e copiando o codigo de aposta para o nó
-    Vetor_MAX[indice] = (NO*)calloc(1,sizeof(NO));
-    Vetor_MIN[indice] = (NO*)calloc(1,sizeof(NO));
-    Vetor_MAX[indice]->codigo_aposta= (char*) calloc(33,sizeof(char));
-    Vetor_MIN[indice]->codigo_aposta= (char*) calloc(33,sizeof(char));
-    strcpy((Vetor_MAX[indice]->codigo_aposta),entrada);
-    strcpy((Vetor_MIN[indice]->codigo_aposta),entrada);
-    
-
-    //pegando cada numero
-    for(int indice_apostados=0;indice_apostados<15;indice_apostados++){
-        fscanf(input,"%d",&num_apostado);
-       
-        /*nums_apostados[indice_apostados]=(int*)malloc(sizeof(int));
-        nums_apostados[indice_apostados]= num_apostado;*/
-        //comparando com os numeros sorteados
-        for (int indice_corretos=0; indice_corretos<10; indice_corretos++){
-            if (num_apostado==sorteados[indice_corretos]){
-                pontuacao+=1;
-                break;}}}
-    
-    //conferindo qual o maior/menor valor de acertos e a quantidade de vencedores
-    if(num_maior==pontuacao){maior+=1;}
-    else{
-      if(num_maior<pontuacao){maior=1;
-    num_maior=pontuacao;}}
-    if(num_menor==pontuacao){menor+=1;}
-    else{if(num_menor>pontuacao){menor=1;
-    num_menor=pontuacao;}}
-    
-    //atribuindo pontuacao a apostas
-    Vetor_MAX[indice]->acertos=pontuacao;
-    Vetor_MIN[indice]->acertos=pontuacao;
-    indice+=1;
-    pontuacao=0;}
-
-
-    //construção do heap e qt de acertos vencedora
-    int ultimo_no_interno = ((qt_apostadores -1)-1)/2;
-    heapify_max(Vetor_MAX,qt_apostadores,ultimo_no_interno);
-    
-    //maior = (Vetor_MAX[0] -> acertos);//onde tiver ponteiros, faça copia dos valores
-    heapify_min(Vetor_MIN,qt_apostadores,ultimo_no_interno);
-    
-
-    //menor = Vetor_MIN[0] -> acertos;
-
-    NO** Ganhadores_max = (NO**)calloc(maior,sizeof(NO*));
-    NO** Ganhadores_min = (NO**)calloc(menor,sizeof(NO*));
-
-  indice -=1;
-  //extraindo para os vetores de ganhadores
-  while ((Vetor_MAX[0] -> acertos)== num_maior){
-
-      extrair_MAX(Vetor_MAX,indice,Ganhadores_max,indice_ganhadores);
-      indice_ganhadores+=1;
-  }
-
-  //definir premio e qt de ganhadores no max
-  int ganhadores_heap_max = indice_ganhadores + 1;
-  int premio_heap_max = ((premio/2)/ganhadores_heap_max);
-
-  //extraindo para vetores de ganhadores
-  indice_ganhadores = 0;
-  while ((Vetor_MIN[0] -> acertos)== num_menor){
-      extrair_MIN(Vetor_MIN,indice,Ganhadores_min,indice_ganhadores);
-      indice_ganhadores+=1;
-  }
-  //definir premio e qt de ganhadores no min
-  int ganhadores_heap_min = indice_ganhadores + 1;
-  int premio_heap_min = ((premio/2)/ganhadores_heap_min);
-
-  //printando para o arquivo de saída
-fprintf(output,"[%d:%d:%d]\n",ganhadores_heap_max,num_maior,premio_heap_max);
-for(int ganhadores=0; ganhadores<ganhadores_heap_max; ganhadores++){
-  fprintf(output,"%s\n",(Ganhadores_max[ganhadores] -> codigo_aposta));
+    return 0;
 }
-fprintf(output,"[%d:%d:%d]\n",ganhadores_heap_min,num_menor,premio_heap_min);
-for(int ganhadores=0; ganhadores<ganhadores_heap_min; ganhadores++){
-  fprintf(output,"%s\n",(Ganhadores_min[ganhadores] -> codigo_aposta));
-}
-fclose(input);
-fclose(output);
-free(Vetor_MAX);
-free(Vetor_MIN);
-free(Ganhadores_max);
-free(Ganhadores_min);
-return 0;}
