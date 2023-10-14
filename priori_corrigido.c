0d60b00dcc1560ebc8a548e8814d18cd82968909
Rafael Miranda <94235393+RafaECUFS@users.noreply.github.com>
2023-10-14 11:39:17 -0300

    Update priori_corrigido.c
    Erro onde o indice passava do tamanho do array corrigido.

--- priori_corrigido.c
+++ priori_corrigido.c
@@ -51,7 +51,7 @@ int main(int argc, char* argv[]) {
     }
 
     for (int index = 0; index < qtd_orgs; index++) {
-        fscanf(input, " %49[^ ] %d\n", lista_orgaos[index].nome_orgao, &lista_orgaos[index].qtd_atend);
+        fscanf(input, " %50s %d\n", lista_orgaos[index].nome_orgao, &lista_orgaos[index].qtd_atend);
         lista_orgaos[index].fila = NULL;
         lista_orgaos[index].alocado = 0;
     } //preenche o array de orgaos
@@ -60,43 +60,54 @@ int main(int argc, char* argv[]) {
     //pega o valor do tamanho maximo da fila
 
     for (int linha = 0; linha < tam_fila; linha++) {
-        fscanf(input, "%[^|]|%[^|]|%d\n", temp_pessoa->nome_orgao, temp_pessoa->nome_pessoa, &temp_pessoa->idade);
+        indice_orgaos=0;
+        fscanf(input, "%50[^|]|%50[^|]|%d\n", temp_pessoa->nome_orgao, temp_pessoa->nome_pessoa, &temp_pessoa->idade);
         printf("oie%s\n", temp_pessoa->nome_orgao);
-        printf("%s\n", temp_pessoa->nome_orgao);
         printf("%s\n", temp_pessoa->nome_pessoa);
         printf("%d\n", linha);
         printf("idade = %d\n", temp_pessoa->idade);
-
-        for (indice_orgaos = 0; indice_orgaos < qtd_orgs; indice_orgaos++) {
-            printf("oi%s\n", lista_orgaos[indice_orgaos].nome_orgao);
-            if (strcmp(temp_pessoa->nome_orgao, lista_orgaos[indice_orgaos].nome_orgao) != 0) {
+        
+        while(strcmp(temp_pessoa->nome_orgao, lista_orgaos[indice_orgaos].nome_orgao )!= 0 && indice_orgaos<qtd_orgs){
+            printf("%sd\n", lista_orgaos[indice_orgaos].nome_orgao);
+            printf("%sd\n", temp_pessoa->nome_orgao);
+            indice_orgaos++;
+            //encontra o orgao igual ao da pessoa
+        }
+        printf("deu erro? %d\n",indice_orgaos);
+        if(strcmp(temp_pessoa->nome_orgao, lista_orgaos[indice_orgaos].nome_orgao)==0 ){printf("igual\n");}
+        if(indice_orgaos>qtd_orgs){
+                printf("Orgao não encontrado\n");
                 continue;
-            }//encontra o orgao igual ao da pessoa
+            }
+        printf("valor = %d\n",lista_orgaos[indice_orgaos].alocado);
             
-            
-
-            if (lista_orgaos[indice_orgaos].alocado==0) {
-                printf("alocadao = %s\n",lista_orgaos[indice_orgaos].nome_orgao);
-                lista_orgaos[indice_orgaos].fila = (pessoa*)calloc(tam_fila, sizeof(pessoa));
-                printf("orgao = %s\n",lista_orgaos[indice_orgaos].nome_orgao);
-                if (lista_orgaos[indice_orgaos].fila == NULL) {
-                    printf("ERRO!");
-                    return 1;
+        if (lista_orgaos[indice_orgaos].alocado==0) {
+            printf("alocadao = %s\n",lista_orgaos[indice_orgaos].nome_orgao);
+            lista_orgaos[indice_orgaos].fila = (pessoa*)calloc(tam_fila, sizeof(pessoa));
+            printf("orgao = %s\n",lista_orgaos[indice_orgaos].nome_orgao);
+            if (lista_orgaos[indice_orgaos].fila == NULL) {
+                printf("ERRO!");
+                return 1;
                 }
-                lista_orgaos[indice_orgaos].alocado=1;
-                printf("alocado = %d\n",lista_orgaos[indice_orgaos].alocado);
-                break;
+            lista_orgaos[indice_orgaos].alocado=1;
+            printf("alocado = %d\n",lista_orgaos[indice_orgaos].alocado);
             }   
             //checa se a fila ja foi alocada. se não, aloca.
-        }printf("nome_pessoas = %s\n",temp_pessoa->nome_pessoa);
 
-        while(lista_orgaos[indice_orgaos].fila[ind_fila].prioridade != 0) {
-            ind_fila+=1;}
+        printf("nome_pessoas = %s\n",temp_pessoa->nome_pessoa);
+        printf("ppp:%d\n",indice_orgaos);
+        printf("ind:%d\n",ind_fila);
+        
+        while(lista_orgaos[indice_orgaos].fila[ind_fila].prioridade != 0 && ind_fila<tam_fila) {
+            printf("priori:%d\n",lista_orgaos[indice_orgaos].fila[ind_fila].prioridade);
+            ind_fila++;}
+        if(ind_fila>tam_fila){printf("ERROW!!!");
+        continue;}
             printf("orgao interesse = %d\n",indice_orgaos);
             printf("nome_pessoa = %s\n",temp_pessoa->nome_pessoa);       
-            memcpy( &lista_orgaos[indice_orgaos].fila[ind_fila],&temp_pessoa, sizeof(pessoa*));
+            memcpy( &lista_orgaos[indice_orgaos].fila[ind_fila],temp_pessoa, sizeof(pessoa));
 
-            printf("nome_pessoa = %d\n",lista_orgaos[indice_orgaos].fila[ind_fila].idade);
+            
 
             if (temp_pessoa->idade < 60) {
                 lista_orgaos[indice_orgaos].fila[ind_fila].prioridade = 1;
@@ -105,7 +116,7 @@ int main(int argc, char* argv[]) {
             }
 
             lista_orgaos[indice_orgaos].fila[ind_fila].ordem = ind_fila;
-
+            printf("pri = %d\n",lista_orgaos[indice_orgaos].fila[ind_fila].prioridade);
             printf("aqui3\n");
             
         }
